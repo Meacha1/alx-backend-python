2636f8e873ef1b8a17f167477cc8f3fe14276eb0
Meacha1 <meachattd@gmail.com>
2023-07-06 15:35:51 +0300

    100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 """
-This module provides a function to safely retrieve the first element from a list.
+This module provides a function to safely
+retrieve the first element from a list.
 """
 
 from typing import Any, List, Optional
+
 
 def safe_first_element(lst: List[Any]) -> Optional[Any]:
     """
@@ -14,10 +16,10 @@ def safe_first_element(lst: List[Any]) -> Optional[Any]:
         lst (List[Any]): The input list.
 
     Returns:
-        Optional[Any]: The first element of the list, or None if the list is empty.
+        Optional[Any]: The first element of the
+        list, or None if the list is empty.
     """
     if lst:
         return lst[0]
     else:
         return None
-
