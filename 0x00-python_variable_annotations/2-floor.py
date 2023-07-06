1325c6a7aa6519820a9dd69eeffa915a9b95eab6
Meacha1 <meachattd@gmail.com>
2023-07-06 14:07:26 +0300

    floot->int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+A script that takes float n as input a retuens a floor of the input
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    Returns a floor of the input
+        args:
+            n: float
+        returns:
+            int: floor of the input
+    '''
+    return int(n)
