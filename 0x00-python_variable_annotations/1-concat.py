fd23e5c054335494ddd38ca95f1b1deb23205758
Meacha1 <meachattd@gmail.com>
2023-07-06 14:00:13 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+'''
+Given two strings and return a concatneted string.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatnets two strings
+        args:
+            str1: first string
+            str2: second string
+        returns:
+            str1 + str2: concatenated string.
+    '''
+    return str1 + str2
