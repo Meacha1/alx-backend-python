9a18153ee59c4606c8bf64d9a87df2bd639b2e69
Meacha1 <meachattd@gmail.com>
2023-07-10 20:32:48 +0300

    Concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,13 +9,15 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay.
 
     Args:
         n (int): The number of times to spawn wait_random.
-        max_delay (int): The maximum delay in seconds for each wait_random call.
+        max_delay (int): The maximum delay in seconds
+        for each wait_random call.
 
     Returns:
         List[float]: The list of delays in ascending order.
