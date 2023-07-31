98dfc465589af9271c15d69d2462094bcb0ef9ef
Meacha1 <meachattd@gmail.com>
2023-07-31 23:38:34 +0300

    1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -33,5 +33,26 @@ class TestAccessNestedMap(unittest.TestCase):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map: dict, path: tuple, exception_type):
+        """
+        Test that the access_nested_map function raises KeyError for specific inputs.
+
+        Args:
+            nested_map (dict): The nested dictionary to access.
+            path (tuple): The path to the desired value in the nested dictionary.
+            exception_type (Exception): The expected exception type.
+
+        Returns:
+            None
+        """
+        with self.assertRaises(exception_type) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), f"Key not found: {path[-1]}")
+
 if __name__ == "__main__":
     unittest.main()
