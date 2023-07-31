44e0a352becbe94a25ca9818b19edc6e0b61ecbf
Meacha1 <meachattd@gmail.com>
2023-08-01 00:50:10 +0300

    5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 
 import unittest
 from unittest.mock import patch
-from parameterized import parameterized_class
+from parameterized import parameterized_class, parameterized
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 from client import GithubOrgClient
 
@@ -11,21 +11,24 @@ from client import GithubOrgClient
 class TestGithubOrgClient(unittest.TestCase):
     """Test cases for GithubOrgClient class"""
 
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
     @patch('client.get_json')
-    def test_org(self, mock_get_json):
+    def test_org(self, org_name, mock_get_json):
         """Test org method"""
         mock_get_json.return_value = self.org_payload
-        org_name = self.org_payload.get('login')
         github_client = GithubOrgClient(org_name)
         org_info = github_client.org()
         self.assertEqual(org_info, self.org_payload)
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
 
-    @patch('client.get_json')
-    def test_public_repos_url(self, mock_get_json):
+    @patch('client.GithubOrgClient.org', new_callable=property)
+    def test_public_repos_url(self, mock_org):
         """Test _public_repos_url property"""
-        mock_get_json.return_value = self.org_payload
-        org_name = self.org_payload.get('login')
-        github_client = GithubOrgClient(org_name)
+        mock_org.return_value = self.org_payload
+        github_client = GithubOrgClient("testorg")
         self.assertEqual(github_client._public_repos_url, self.org_payload.get('repos_url'))
 
     @patch('client.get_json')
@@ -40,7 +43,7 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with("https://api.github.com/orgs/testorg/repos")
 
-    @parameterized_class("repo", "license_key", "expected_has_license", [
+    @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
@@ -73,6 +76,20 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
     def test_integration_public_repos(self):
         """Integration test for public_repos method"""
+        org_name = self.org_payload.get('login')
+        github_client = GithubOrgClient(org_name)
+        repos = github_client.public_repos(license="apache-2.0")
+        self.assertEqual(repos, apache2_repos)
+
+    def test_public_repos(self):
+        """Integration test for public_repos method using fixtures"""
+        org_name = self.org_payload.get('login')
+        github_client = GithubOrgClient(org_name)
+        repos = github_client.public_repos()
+        self.assertEqual(repos, expected_repos)
+
+    def test_public_repos_with_license(self):
+        """Integration test for public_repos method with license argument using fixtures"""
         org_name = self.org_payload.get('login')
         github_client = GithubOrgClient(org_name)
         repos = github_client.public_repos(license="apache-2.0")
