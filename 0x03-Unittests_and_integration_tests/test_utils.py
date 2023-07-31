a117242a00bea7371cabd9638bd3dde88fd1bd89
Meacha1 <meachattd@gmail.com>
2023-07-31 23:54:24 +0300

    2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-"""
-This module contains unit tests for the utils functions.
-"""
-
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -65,19 +61,21 @@ class TestGetJson(unittest.TestCase):
         ("http://holberton.io", {"payload": False}),
     ])
     @patch('utils.requests.get')
-    def test_get_json(self, test_url: str, test_payload: dict, mock_get):
+    def test_get_json(self, test_url, test_payload, mock_get):
         """
         Test the get_json function with mocked HTTP calls.
 
         Args:
-            test_url (str): The URL to be used for testing.
-            test_payload (dict): The expected JSON payload.
-            mock_get: The mocked requests.get function.
+            test_url (str): The URL to be used in the test.
+            test_payload (dict): The expected payload to be returned.
+            mock_get (MagicMock): The mocked get method from requests module.
 
         Returns:
             None
         """
+        mock_get.return_value = Mock()
         mock_get.return_value.json.return_value = test_payload
+
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
         mock_get.assert_called_once_with(test_url)
@@ -88,6 +86,7 @@ class TestMemoize(unittest.TestCase):
     """
 
     class TestClass:
+
         def a_method(self):
             return 42
 
@@ -95,21 +94,26 @@ class TestMemoize(unittest.TestCase):
         def a_property(self):
             return self.a_method()
 
-    def test_memoize(self):
+    @patch.object(TestClass, 'a_method')
+    def test_memoize(self, mock_a_method):
         """
-        Test that the memoize decorator caches the result.
+        Test the memoize decorator.
+
+        Args:
+            mock_a_method (MagicMock): The mocked a_method.
 
         Returns:
             None
         """
-        test_instance = self.TestClass()
-        with patch.object(test_instance, 'a_method') as mock_method:
-            result_1 = test_instance.a_property
-            result_2 = test_instance.a_property
+        instance = TestClass()
+        mock_a_method.return_value = 42
 
-            self.assertEqual(result_1, 42)
-            self.assertEqual(result_1, result_2)
-            mock_method.assert_called_once()
+        result1 = instance.a_property
+        result2 = instance.a_property
+
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
+        mock_a_method.assert_called_once()
 
 if __name__ == "__main__":
     unittest.main()
