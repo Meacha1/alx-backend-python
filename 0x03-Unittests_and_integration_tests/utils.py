11b111af43ea7c50c4f9d1476eabadece867c8fd
Meacha1 <meachattd@gmail.com>
2023-08-01 00:20:28 +0300

    3

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,75 +1,62 @@
 #!/usr/bin/env python3
-"""Generic utilities for github org client.
-"""
-import requests
-from functools import wraps
-from typing import (
-    Mapping,
-    Sequence,
-    Any,
-    Dict,
-    Callable,
-)
+"""Unit tests for utils.py"""
 
-__all__ = [
-    "access_nested_map",
-    "get_json",
-    "memoize",
-]
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
 
+class TestUtils(unittest.TestCase):
+    """Test cases for utils.py"""
 
-def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
-    """Access nested map with key path.
-    Parameters
-    ----------
-    nested_map: Mapping
-        A nested map
-    path: Sequence
-        a sequence of key representing a path to the value
-    Example
-    -------
-    >>> nested_map = {"a": {"b": {"c": 1}}}
-    >>> access_nested_map(nested_map, ["a", "b", "c"])
-    1
-    """
-    for key in path:
-        if not isinstance(nested_map, Mapping):
-            raise KeyError(key)
-        nested_map = nested_map[key]
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """Test access_nested_map function"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
-    return nested_map
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test get_json function"""
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = test_payload
 
+        result = get_json(test_url)
 
-def get_json(url: str) -> Dict:
-    """Get JSON from remote URL.
-    """
-    response = requests.get(url)
-    return response.json()
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
+    class TestClass:
+        """Test class for memoize decorator"""
 
-def memoize(fn: Callable) -> Callable:
-    """Decorator to memoize a method.
-    Example
-    -------
-    class MyClass:
-        @memoize
         def a_method(self):
-            print("a_method called")
+            """Test method for memoize decorator"""
             return 42
-    >>> my_object = MyClass()
-    >>> my_object.a_method
-    a_method called
-    42
-    >>> my_object.a_method
-    42
-    """
-    attr_name = "_{}".format(fn.__name__)
 
-    @wraps(fn)
-    def memoized(self):
-        """"memoized wraps"""
-        if not hasattr(self, attr_name):
-            setattr(self, attr_name, fn(self))
-        return getattr(self, attr_name)
+        @memoize
+        def a_property(self):
+            """Test property for memoize decorator"""
+            return self.a_method()
 
-    return property(memoized)
+    def test_memoize(self):
+        """Test memoize decorator"""
+        my_object = self.TestClass()
+
+        with patch.object(my_object, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+
+            result1 = my_object.a_property
+            result2 = my_object.a_property
+
+            mock_a_method.assert_called_once()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+if __name__ == "__main__":
+    unittest.main()
